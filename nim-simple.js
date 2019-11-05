a778c5a9acf5036dc0babe59607d9eef645955c3
Novakm45 <54716094+Novakm45@users.noreply.github.com>
2019-11-05 11:40:52 -0500

    Add files via upload

---
+++ nim-simple.js
@@ -0,0 +1,107 @@
+/*Nim Simple By Max*/
+
+/*global Variables */
+var games = 0;
+var count = 0;
+var turns = 0;
+/* main */
+var next = 0;
+//Next Game Loop
+
+/* Functions */
+
+// CPU Turn
+
+//PC Turn
+
+Function userTurn(){
+	return 2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
